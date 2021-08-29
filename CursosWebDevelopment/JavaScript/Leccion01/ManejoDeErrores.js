b0a65eac7b8a5c9476ee0687604cdf58953cb023
Abraham Cruz <ddfkr2894@gmail.com>
2021-08-29 16:13:32 -0500

    Create ManejoDeErrores.js

---
+++ CursosWebDevelopment/JavaScript/Leccion01/ManejoDeErrores.js
@@ -0,0 +1,33 @@
+'use strict'
+try{
+    // let x = 10;
+    miFuncion();
+}
+catch(error){
+    console.log(error);
+}
+finally{
+    console.log('termina la revisión de errores');
+}
+
+console.log('continuamos...');
+
+// -----------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------
+let resultado = -1;
+
+try{
+    if(isNaN(resultado)) throw 'No es un número';
+    else if( resultado === '') throw 'Es cadena vacía';
+    else if( resultado >= 0) throw 'Valor positivo';
+    else if( resultado < 0 ) throw 'Valor negativo';
+}
+catch(error){
+    console.log(error);
+    //console.log(error.name);
+    //console.log(error.message);
+}
+finally{
+    console.log('Termina revisión de errores');
+}
