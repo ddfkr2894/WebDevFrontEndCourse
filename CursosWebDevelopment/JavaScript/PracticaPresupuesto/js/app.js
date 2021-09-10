ebdfe9ebc110ffae7cc463fbbee472c2959b4309
Abraham Cruz <ddfkr2894@gmail.com>
2021-09-10 01:04:51 -0500

    Update app.js

--- CursosWebDevelopment/JavaScript/PracticaPresupuesto/js/app.js
+++ CursosWebDevelopment/JavaScript/PracticaPresupuesto/js/app.js
@@ -1,5 +1,5 @@
 const ingresos = [
-    new Ingreso('Salario', 5000.00),
+    new Ingreso('Salario', 8000.00),
     new Ingreso('Venta de Coche', 1500)
 ];
 
@@ -32,8 +32,16 @@ let cargarEncabezado = () =>{
     let presupuesto = totalIngresos() - totalEgresos();
     let porcentajeEgreso = totalEgresos() / totalIngresos();
     console.log(presupuesto);
-    document.getElementById('presupuesto').innerHTML = presupuesto;
-    document.getElementById('porcentaje').innerHTML = porcentajeEgreso;
-    document.getElementById('ingresos').innerHTML = totalIngresos();
-    document.getElementById('egresos').innerHTML = totalEgresos();
+    document.getElementById('presupuesto').innerHTML = formatoMoneda(presupuesto);
+    document.getElementById('porcentaje').innerHTML = formatoPorcentaje(porcentajeEgreso);
+    document.getElementById('ingresos').innerHTML = formatoMoneda(totalIngresos());
+    document.getElementById('egresos').innerHTML = formatoMoneda(totalEgresos());
+}
+
+const formatoMoneda = (valor) =>{
+    return valor.toLocaleString('en-US',{style: 'currency', currency:'USD', minimumFractionDigits:2});
+}
+
+const formatoPorcentaje = (valor) =>{
+    return valor.toLocaleString('en-US',{style:'percent', minimumFractionDigits:2});
 }
